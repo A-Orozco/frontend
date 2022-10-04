d2e5b7812336950586f1a281f6bb81a7c7db7170
Aaron <a.k.orozco808@gmail.com>
2022-10-03 22:20:28 -0500

    g

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
 // functional component with hooks is more common
 const App= () => {
   return (
     //output must be wrapped in only 1 element
     <>
-      <h1>Welcome</h1>
-    </>
+      <Header />
+      <main>  
+        <h1>hai 2 u . com</h1>
+      </main>
+      <Footer />
+    </> 
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+// rafce to create template
+import React from 'react'
+
+const Header = () => {
+  return (
+    <hearder>hed</hearder>
+  )
+}
+
+export default Header
