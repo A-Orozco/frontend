e2cae546ed082406229080c96b7f61f29819566f
Aaron <a.k.orozco808@gmail.com>
2022-10-03 21:27:37 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+// functional component with hooks is more common
+const App= () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //output must be wrapped in only 1 element
+    <>
+      <h1>Welcome</h1>
+    </>
   );
 }
 
